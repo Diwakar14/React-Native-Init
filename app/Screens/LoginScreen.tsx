3cb889d04a647821f0d0eefab579f779dd27e70a
Diwakar Prasad <diwakarp1999@gmail.com>
2020-12-31 15:42:52 +0530

    Initial Commit

---
+++ app/Screens/LoginScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+
+const LoginScreen = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hello People</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default LoginScreen;
